1ad28dc2993aad937eafe1fee436f53fcdd63b95
musaagca131@gmail.com <musaagca131@gmail.com>
2026-07-16 08:55:41 +0300

    Kategori sayfası düzenlendi

--- YemekhaneStokTakipV2/Pages/Index.cshtml.cs
+++ YemekhaneStokTakipV2/Pages/Index.cshtml.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 using YemekhaneStokTakipV2.Data;
+using YemekhaneStokTakipV2.Models;
 
 namespace YemekhaneStokTakipV2.Pages
 {
@@ -18,6 +20,9 @@ namespace YemekhaneStokTakipV2.Pages
         public int ToplamStokGiris { get; set; }
         public int ToplamStokCikis { get; set; }
 
+        // Kritik stoktaki ürünler
+        public IList<Urun> KritikUrunler { get; set; } = new List<Urun>();
+
         public void OnGet()
         {
             ToplamUrun = _context.Urunler.Count();
@@ -25,6 +30,11 @@ namespace YemekhaneStokTakipV2.Pages
             ToplamTedarikci = _context.Tedarikciler.Count();
             ToplamStokGiris = _context.StokGiris.Count();
             ToplamStokCikis = _context.StokCikis.Count();
+
+            KritikUrunler = _context.Urunler
+                .Include(x => x.Kategori)
+                .Where(x => x.MevcutStok <= x.KritikStok)
+                .ToList();
         }
     }
 }
